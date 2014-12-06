572389e4b60ffe8b404a5ffcaefca998e4772eae
Sascha Homeier <shomeier@meyle-mueller.de>
2014-12-07 00:53:06 +0100

    Added RAP business css theme to RAP target

--- org.shorty.e4.master.ui.rap/src/main/java/org/shorty/e4/master/ui/rap/BasicApplication.java
+++ org.shorty.e4.master.ui.rap/src/main/java/org/shorty/e4/master/ui/rap/BasicApplication.java
@@ -10,18 +10,21 @@ import org.eclipse.rap.rwt.application.Application.OperationMode;
 import org.eclipse.rap.rwt.application.ApplicationConfiguration;
 import org.eclipse.rap.rwt.client.WebClient;
 
-public class BasicApplication implements ApplicationConfiguration {
+public class BasicApplication implements ApplicationConfiguration
+{
 
-	public void configure(Application application) {
+	public void configure(Application application)
+	{
+		application.addStyleSheet("rap.business", "theme/business/business.css");
+		// application.addStyleSheet("shorty.business", "theme/business/shorty_busy.css");
 		Map<String, String> properties = new HashMap<String, String>();
-		properties.put(WebClient.PAGE_TITLE, "Hello e4 RAP");
+		properties.put(WebClient.PAGE_TITLE, "e4 RAP App");
+		properties.put(WebClient.THEME_ID, "rap.business");
 		application
-				.addEntryPoint(
-						"/rap",
-						new E4EntryPointFactory(
-								E4ApplicationConfig
-										.create("platform:/plugin/org.shorty.e4.master.ui.rap/Application.e4xmi")),
-						properties);
+			.addEntryPoint(
+				"/rap",
+				new E4EntryPointFactory(E4ApplicationConfig
+					.create("platform:/plugin/org.shorty.e4.master.ui.rap/Application.e4xmi")), properties);
 		application.setOperationMode(OperationMode.SWT_COMPATIBILITY);
 	}
 
