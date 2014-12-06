08bdd9ee71c6da971a409a44d9acc62941ac24ce
Sascha Homeier <shomeier@meyle-mueller.de>
2014-12-06 22:26:41 +0100

    Introduced Toolbar with Item and ExitHandler
    Also included it in RAP Application model.

---
+++ org.shorty.e4.common.ui/src/main/java/org/shorty/e4/commons/ui/handler/ExitHandler.java
@@ -0,0 +1,15 @@
+package org.shorty.e4.commons.ui.handler;
+
+import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.e4.ui.workbench.IWorkbench;
+
+public class ExitHandler
+{
+
+	@Execute
+	public void execute(IWorkbench workbench)
+	{
+		workbench.close();
+	}
+
+}
